3a7a3de62c4eaebf6c66217a7a951cdd48b21cd1
aaug <aaug@caasco.ca>
2021-05-21 16:11:43 -0400

    rendering works well but neets refactoring

--- js/main.js
+++ js/main.js
@@ -59,7 +59,44 @@ let scoFf = {
 
         console.log(listDataActive);
 
-        divShowData.innerHTML = JSON.stringify(listDataActive);
+        // Extract value from table header.
+        // ('Book ID', 'Book Name', 'Category' and 'Price')
+        let col = [];
+        for (let i = 0; i < listDataActive.length; i++) {
+            for (let key in listDataActive[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // Create a table.
+        const table = document.createElement("table");
+        // Create table header row using the extracted headers above.
+        let tr = table.insertRow(-1); // table row.
+
+        for (let i = 0; i < col.length; i++) {
+            let th = document.createElement("th"); // table header.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+
+        }
+
+        // add json data to the table as rows.
+        for (let i = 0; i < listDataActive.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (let j = 0; j < col.length; j++) {
+                let tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = listDataActive[i][col[j]];
+            }
+        }
+
+        // Now, add the newly created table with json data, to a container.
+        // const divShowData = document.getElementById('showData');
+        divShowData.innerHTML = "";
+        divShowData.appendChild(table);
 
     },
 
@@ -70,9 +107,45 @@ let scoFf = {
         var listDataActiveResult = listDataActive.filter(obj => obj.status == "active");
 
         console.log(listDataActive);
-        console.log(listDataActiveResult);
 
-        divShowData.innerHTML = JSON.stringify(listDataActiveResult);
+        // Extract value from table header.
+        // ('Book ID', 'Book Name', 'Category' and 'Price')
+        let col = [];
+        for (let i = 0; i < listDataActiveResult.length; i++) {
+            for (let key in listDataActiveResult[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // Create a table.
+        const table = document.createElement("table");
+        // Create table header row using the extracted headers above.
+        let tr = table.insertRow(-1); // table row.
+
+        for (let i = 0; i < col.length; i++) {
+            let th = document.createElement("th"); // table header.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+
+        }
+
+        // add json data to the table as rows.
+        for (let i = 0; i < listDataActiveResult.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (let j = 0; j < col.length; j++) {
+                let tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = listDataActiveResult[i][col[j]];
+            }
+        }
+
+        // Now, add the newly created table with json data, to a container.
+        // const divShowData = document.getElementById('showData');
+        divShowData.innerHTML = "";
+        divShowData.appendChild(table);
 
 
 
@@ -96,9 +169,45 @@ let scoFf = {
         var listCompleteResult = listDataActive.filter(obj => obj.status == "completed");
 
         console.log(listDataActive);
-        console.log(listCompleteResult);
 
-        divShowData.innerHTML = JSON.stringify(listCompleteResult);
+        // Extract value from table header.
+        // ('Book ID', 'Book Name', 'Category' and 'Price')
+        let col = [];
+        for (let i = 0; i < listCompleteResult.length; i++) {
+            for (let key in listCompleteResult[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // Create a table.
+        const table = document.createElement("table");
+        // Create table header row using the extracted headers above.
+        let tr = table.insertRow(-1); // table row.
+
+        for (let i = 0; i < col.length; i++) {
+            let th = document.createElement("th"); // table header.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+
+        }
+
+        // add json data to the table as rows.
+        for (let i = 0; i < listCompleteResult.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (let j = 0; j < col.length; j++) {
+                let tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = listCompleteResult[i][col[j]];
+            }
+        }
+
+        // Now, add the newly created table with json data, to a container.
+        // const divShowData = document.getElementById('showData');
+        divShowData.innerHTML = "";
+        divShowData.appendChild(table);
 
         // console.log("*---*");
     },
