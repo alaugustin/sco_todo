e2e2381dc29636ea0bc7d253f33c81d9d349b3d6
aaug <aaug@caasco.ca>
2021-05-21 12:36:51 -0400

    inputSave()

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 console.log("linked in");
         scoFf.footerDate();
+        scoFf.inputSave();
     footerDate: () => {
         // console.log("wired in");
         let dateHolder = document.getElementById("dateHolder"),
             d = new Date().getFullYear();
         dateHolder.innerHTML = d;
     },
+    inputSave: () => {
+        const addButton = document.getElementById("mainButton");
+
+        let foo = document.getElementsByTagName("input");
+
+
+        addButton.addEventListener("click", function(){
+            console.log(foo);
+        });
+    }
