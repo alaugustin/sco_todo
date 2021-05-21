c04430910ab0072bda44f28c623a101b304dbc0f
aaug <aaug@caasco.ca>
2021-05-21 12:36:12 -0400

    footerDate()

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 console.log("linked in");
+        scoFf.footerDate();
+    footerDate: () => {
+        // console.log("wired in");
+        let dateHolder = document.getElementById("dateHolder"),
+            d = new Date().getFullYear();
+        dateHolder.innerHTML = d;
+    },
